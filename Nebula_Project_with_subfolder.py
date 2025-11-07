43a00d4f293c3a55da03ff3fdfd5f51f31dff7f5
nithyaarivu <143952146+nithyaarivu@users.noreply.github.com>
2025-11-07 09:23:45 +0400

    Add files via upload

---
+++ Nebula_Project_with_subfolder.py
@@ -0,0 +1,10 @@
+
+# hello.py
+print("Hello, World! Welcome to Python in PyCharm 🚀")
+a = 12
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
